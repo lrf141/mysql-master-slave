0809a47cf01b5ee39e997274331762bc7c3662cc
lrf141 <ghost141.kentyo@gmail.com>
2019-12-31 22:16:58 +0900

    コンフィグを修正

--- docker/slave1-entrypoint-initdb/slave-launch-script.sh
+++ docker/slave1-entrypoint-initdb/slave-launch-script.sh
@@ -1,15 +1,13 @@
-#!/bin/shi
+#!/bin/sh
 
 while ! mysqladmin ping -h master --silent; do
     sleep 1
 done
 
-if [ -v MYSQL_REPLICATION_USER -a -v MYSQL_REPLICATION_PASSWORD]; then
-    mysql -u root -v mysql <<SQL
-        CREATE USER '$MYSQL_REPLICATION_USER'@'%' IDENTIFIED BY '$MYSQL_REPLICATION_PASSWORD';
+mysql -u root -v mysql <<SQL
+        CREATE USER '${MYSQL_REPLICATION_USER:+repl}'@'%' IDENTIFIED BY '${MYSQL_REPLICATION_PASSWORD:+repl}';
         GRANT REPLICATION SLAVE ON *.* TO '$MYSQL_REPLICATION_USER'@'%';
 SQL
-fi
 mysql -u root -v mysql <<SQL
     RESET MASTER;
     FLUSH TABLES WITH READ LOCK;
@@ -19,8 +17,8 @@ position=`mysql -u root -h master -e "SHOW MASTER STATUS\G" | grep Position: | a
 
 mysqldump -u root -h master --all-databases --master-data > /tmp/master.sql
 mysql -u root < /tmp/master.sql
-
-mysql -u root -v -e "RESET SLAVE";
+mysql -u root -v -e "STOP SLAVE;"
+mysql -u root -v -e "RESET SLAVE;"
 mysql -u root -v -e "CHANGE MASTER TO MASTER_HOST='master', MASTER_USER='root', MASTER_PASSWORD='', MASTER_LOG_FILE='${binlogfile}', MASTER_LOG_POS=${position};"
 mysql -u root -v -e "START SLAVE;"
 
